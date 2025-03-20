c2f3a8301ab71dec6831df0483d4bf6a2a2e8d2c
apolock33 <carlosammgomes@gmail.com>
2025-03-20 13:03:49 -0300

    Margin Ajustments

--- src/components/sections/projects.jsx
+++ src/components/sections/projects.jsx
@@ -8,8 +8,12 @@ const Projects = () => {
 
   return (
     <motion.section id='projects' className={`about-section flex flex-column align-items-center justify-content-center ${isMobile ? null : 'gap-8'}`} initial={{ opacity: 0 }} animate={{ opacity: 1, transition: { duration: 1 } }}>
-      <div className='flex flex-column justify-content-center text-center py-8'>
-        <h1 className='font-bold text-4xl text-primary m-0 text-center' style={{ fontFamily: 'var(--title-font) !important' }}>Projetos</h1>
+      <div className='flex flex-column justify-content-center text-center pt-8'>
+        <h1
+          className='font-bold text-4xl text-primary m-0 text-center'
+          style={{ fontFamily: 'var(--title-font) !important' }}>
+          Projetos
+        </h1>
         <p>Aqui estão alguns dos projetos que participei e concluí ao longo da minha carreira</p>
       </div>
       <ProjectCardsTable />
